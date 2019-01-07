e04db15bac73d580b9ba4c3d71ccf3ee4506fe16
Chris Evans <Cevans@peerlessproducts.com>
2019-01-07 09:25:25 -0600

    added neural network module

---
+++ ANN.Net/NeuralNetwork.fs
@@ -0,0 +1,8 @@
+﻿module NeuralNetworks =
+    open System
+    type Node = {Value:float;NormalizeFunction:Func<float,float>}
+    type Weight = {Value:float}
+    type Connection = {Input:Node;Weight:Weight;Output:Node}
+    type NetworkLayer = {Nodes:Node[]}
+
+
